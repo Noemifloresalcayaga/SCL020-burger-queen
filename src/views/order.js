75d51f38534e4111696537df968dd49e6973b516
Sol <solemich@gmail.com>
2022-07-21 16:36:34 -0400

    boton desplegable

--- src/views/order.js
+++ src/views/order.js
@@ -1,20 +1,63 @@
-import {Link} from "react-router-dom";
+import {useState, useRef} from "react";
+import style from "./order.css";
+import { useDetectOutsideClick } from "./useDetectOutsideClick.js";
+
+
 
 function Mesero() {
+  //Funcion menu desplegable
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => setIsActive(!isActive);
+  //Funcion para mostrar cliente
+    const [save, setSave] = useState("");
     return (
     <>
       <div className="Mesero">
         <h2>Nombre del cliente:</h2>
-        <button id="postres">Entradas</button>
-        <button id="postres">Hamburguesas</button>
-        <button id="postres">Acompañamientos</button>
-        <button id="postres">Postres</button>
+      </div>
+        <form onSubmit={ev => {
+          ev.preventDefault();
+          setSave(ev.target.save.value);  
+        }}
+        >
+        <input type="text" name="save" autoComplete="off"></input>
+        <button className="postres" type="submit">Ingresar cliente</button>
+        <p>Pedido de: {save}</p>
+        </form>
+        <button className="postres" id="postres">Entradas</button>
+        <button className="postres" id="postres">Hamburguesas</button>
+        <button className="postres" id="postres">Acompañamientos</button>
+        <button className="postres" id="postres">Postres</button>
         <div>
          <p>Items:</p>   
-         <button id="postres">Enviar</button>
+         <button className="postres" id="postres">Enviar</button>
          <p>Total:$</p>  
-        </div>
+    <div className="container">
+      <div className="menu-container">
+        <button onClick={onClick} className="menu-trigger">
+          <span className="postres">Postres</span>
+        </button>
+        <nav
+          ref={style.dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
+        >
+          <ul>
+            <li>
+              <a href="#">Pie de Limón</a>
+            </li>
+            <li>
+              <a href="#">Brownie VeggieHappy</a>
+            </li>
+            <li>
+              <a href="#">Flan de tofu</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+    </div>
+  </div>
     </>
    )}
+
    export default Mesero
