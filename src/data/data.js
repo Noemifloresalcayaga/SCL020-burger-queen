c55f8b1f19ded86626106ce6ae378fb09205cb15
Noemí Flores <noemifloryaga@gmail.com>
2022-07-26 11:11:35 -0400

    data

---
+++ src/data/data.js
@@ -0,0 +1,79 @@
+data = [
+    
+      {
+        id: 1,
+        nombre: "Pie de limon",
+        cantidad:0,
+        precio: 2500,
+      },
+      {
+        id: 2,
+        nombre: "Brownie VeggieHappy",
+        cantidad:0,
+        precio: 3500
+      },
+      {
+        id: 3,
+        nombre: "Flan de tofu",
+        cantidad:0,
+        precio: 2500
+      },
+      {
+        id: 4,
+        nombre: "Sopa de tofu",
+        cantidad:0,
+        precio: 4500
+      },
+      {
+        id: 5,
+        nombre: "Palta reina",
+        cantidad:0,
+        precio: 7500
+      },
+      {
+        id: 6,
+        nombre: "Happy bowl",
+        cantidad:0,
+        precio: 7500
+      },
+      {
+        id: 7,
+        nombre: "Seitan Burguer",
+        cantidad:0,
+        precio: 6000
+      },
+      {
+        id: 8,
+        nombre: "Tofu Burguer",
+        cantidad:0,
+        precio: 6000
+      },
+      {
+        id: 9,
+        nombre: "Falafel Burguer",
+        cantidad:0,
+        precio: 6000
+      },
+      {
+        id: 10,
+        nombre:  "Papas fritas",
+        cantidad:0,
+        precio: 2500
+      },
+      {
+        id: 11,
+        nombre: "Champiñones crujientes",
+        cantidad:0,
+        precio: 3500
+      },
+      
+      {
+        id: 12,
+        nombre: "Aros de cebolla",
+        cantidad:0,
+        precio: 3500
+      },
+
+    ];
+ 
+  
