1b08c3dacee9f4a2d482f1180d281f4790993f21
Sol <solemich@gmail.com>
2022-07-27 16:08:11 -0400

    css

--- src/data/data.js
+++ src/data/data.js
@@ -1,81 +1,3 @@
-<<<<<<< HEAD
-data = [
-    
-      {
-        "id": 1,
-        "nombre": "Pie de limon",
-        "amount":0,
-        "precio": 2500,
-       
-      },
-      {
-        "id": 2,
-        "nombre": "Brownie VeggieHappy",
-        "amount":0,
-        "precio": 3500
-      },
-      {
-        "id": 3,
-        "nombre": "Flan de tofu",
-        "amount":0,
-        "precio": 2500
-      },
-      {
-        "id": 4,
-        "nombre": "Sopa de tofu",
-        "amount":0,
-        "precio": 4500
-      },
-      {
-        "id": 5,
-        "nombre": "Palta reina",
-        "amount":0,
-        "precio": 7500
-      },
-      {
-        "id": 6,
-        "nombre": "Happy bowl",
-        "amount":0,
-        "precio": 7500
-      },
-      {
-        "id": 7,
-        "nombre": "Seitan Burguer",
-        "amount":0,
-        "precio": 6000
-      },
-      {
-        "id": 8,
-        "nombre": "Tofu Burguer",
-        "amount":0,
-        "precio": 6000
-      },
-      {
-        "id": 9,
-        "nombre": "Falafel Burguer",
-        "amount":0,
-        "precio": 6000
-      },
-      {
-        "id": 10,
-        "nombre":  "Papas fritas",
-        "amount":0,
-        "precio": 2500
-      },
-      {
-        "id": 11,
-        "nombre": "Champiñones crujientes",
-        "amount":0,
-        "precio": 3500
-      },
-      
-      {
-        "id": 12,
-        "nombre": "Aros de cebolla",
-        "amount":0,
-        "precio": 3500
-      },
-=======
 data = {
   Postres: [
     {
@@ -150,7 +72,6 @@ data = {
       amount: 0,
       price: 3500,
     },
->>>>>>> 67f40169c4bd6353c86f2b99f1ffdb604701fcd6
 
     {
       id: 12,
