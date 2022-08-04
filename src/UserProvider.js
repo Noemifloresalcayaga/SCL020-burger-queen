e411c169520c39cfd157ecd8dc554a70f6166836
Sol <solemich@gmail.com>
2022-08-03 23:18:45 -0400

    testing

--- src/UserProvider.js
+++ src/UserProvider.js
@@ -1,26 +1,39 @@
-// // import React, {useState, useContext} from "react";
+// import React, {useState, useContext} from "react";
 
-// const userContext = React.createContext();
-// const userToggleContext = React.createContext();
+const userContext = React.createContext();
+const userToggleContext = React.createContext();
 
-// export function useUserContext(){
-//     return useContext(userContext);
-// }
+export function useUserContext(){
+    return useContext(userContext);
+}
 
-// export function useUserToggleContext(){
-//     return useContext(userToggleContext);
-// }
+export function useUserToggleContext(){
+    return useContext(userToggleContext);
+}
 
 
-// export function UserProvider(props){
-   
+export function UserProvider(props){
 
-//     return(
-//         <userContext.Provider value={save}>
-//             <userToggleContext.Provider value={handleSubmit}>
-//                 {props.children}
-//             </userToggleContext.Provider>
-//         </userContext.Provider>
+  const [user, setUser] = useState(null);
+  const cambiaLogin = () => {
+      if(user){
+        setUser(null);
+      }
+      else{
+        setUser({
+          name: "Noe y cielo",
+          mail: "cielo@gmail.com"
+        });
+      }
+    }
+     
 
-//     );
-// }
+    return(
+        <userContext.Provider value={save}>
+            <userToggleContext.Provider value={handleSubmit}>
+                {props.children}
+            </userToggleContext.Provider>
+        </userContext.Provider>
+
+    );
+}
